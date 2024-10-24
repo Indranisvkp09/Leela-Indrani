ccb768ec0e41efe716b59702245ec9bfc114cf87
harid <harid@DESKTOP-OIS9IKL.lan>
2024-10-24 21:05:48 +0530

    second commit

---
+++ SVKP_college/src/basicsofjava/DatatypeDemo.java
@@ -0,0 +1,24 @@
+package basicsofjava;
+
+public class DatatypeDemo {
+
+	public static void main(String[] args) {
+		
+		//byte - 1 byte
+		 
+		byte byteMax = 127;
+		byte byteMin = -128;
+		System.out.println("Min range of byte is" +byteMin +"Max range of byte" + byteMax);
+		
+		//short - 2 bytes
+		 
+		byte byteMax = 127;
+		byte byteMin = -128;
+		System.out.println("Min range of byte is" +byteMin +"Max range of byte" + byteMax);
+				
+		
+		
+   
+	}
+
+}
