51a9ed792ea16812cc44ed8e99121082b9efdfa2
harid <harid@DESKTOP-OIS9IKL.lan>
2024-12-12 20:47:49 +0530

    first commit

---
+++ SVKP_college/src/org/tnsif/capgemini/c2tc/Sr_developer/KIA_Internal.java
@@ -0,0 +1,14 @@
+package org.tnsif.capgemini.c2tc.Sr_developer;
+import org.tnsif.capgemini.c2tc.Jr_developer.*;
+public class KIA_Internal {
+
+	public static void main(String[] args) {
+		
+		KIA_AirBags obj=new KIA_AirBags();
+		obj.disp_AirBags();
+		
+//		 KIA_Internal obj1=new  KIA_Internal();
+//		obj1.disp_Speed_limit();
+	}
+
+}
